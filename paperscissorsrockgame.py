aa1da2f58bbe3d97e594c8fa34d6b1ee13b327d1
jacob <rosej@stu.otc.school.nz>
2020-10-06 15:32:18 +1300

    added a feature that allows user to go back to selection

--- paperscissorsrockgame.py
+++ paperscissorsrockgame.py
@@ -1,4 +1,5 @@
 from random import randint
+back_list=["back","go back","selection"]
 def game():
     #rock paper or scissors optios
     t = ["rock", "paper", "scissors"]
@@ -11,7 +12,7 @@ def game():
     print("Again, please answer in lowercase")
     while player == False:
     #set player to True
-        player = input("paper, scissors, or rock?")
+        player = input("paper, scissors, or rock? Or do you want to go back to the selection")
         if player == bearbot:
             print("It's a tie!")
         elif player == "rock":
@@ -29,10 +30,12 @@ def game():
                 print("You lose...", bearbot, "smashes", player)
             else:
                 print("You win!", player, "cut", bearbot)
+        elif player in back_list:
+            import maincentre
         else:
             print("That is an incorrect answer, please input either rock, paper or scissors!")
-        #player was set to True, but we want it to be False so the loop continues/kinda dont ge this luv
         player = False
         bearbot = t[randint(0,2)]
 
+game()
 
