630de3bc22ab3c5a154c645f07e339db0a02d24b
jacob <rosej@stu.otc.school.nz>
2020-10-06 15:32:00 +1300

    fixed it so it doesnt only run bearbot

--- maincentre.py
+++ maincentre.py
@@ -1,24 +1,34 @@
-#imports
-import berabot
-import gamestory
-import paperscissorsrockgame
-game_list = ["game","I want to play a game","i want play game","i want game play"]
-paperscissorsrock = ["paper scissors rock"]
-horror_story = ["horror story","game story"]
+#import
+game_list = ["game","I want to play a game","i want play game","i want game play","play a game","i want to play a game","i wanna play a game"]
+paperscissorsrock = ["paper scissors rock","psr"]
+horror_story = ["horror story","game story","horror game","horror","horror story game"]
 bearbot_list = ["bearbot", "Bearbot"]
 chatterbot_list = ["chatterbot","Chatterbot"]
 
-choice = input("Would you like to talk to Bearbot or play a Game?")
-if choice in bearbot_list:
-    berabot
-elif choice in game_list:
+
+def gameoptions():
     whatgame = input("What game would you like to play? Paper, scissors, rock, or a horror story game?")
     if whatgame in horror_story:
-        gamestory
+        import gamestory
     elif whatgame in paperscissorsrock:
-        paperscissorsrockgame
+        import paperscissorsrockgame
     else:
         print("Please enter a valid response")
+        gameoptions()
+print("Would you like to talk to Bearbot or play a Game?")
+def choices():
+    choice = input()
+    if choice in bearbot_list:
+        import berabot
+    elif choice in game_list:
+        gameoptions()
+    else:
+        print("Please enter a valid response")
+        choices()
+
+choices()
+
+
 
 
 
