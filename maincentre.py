2ff433e9202966d4c666524c58f0f05c385e7663
jacob <rosej@stu.otc.school.nz>
2020-10-11 22:53:46 +1300

    added feature that allows user to go back after choosing a game, incase they change their mind

--- maincentre.py
+++ maincentre.py
@@ -4,6 +4,8 @@ paperscissorsrock = ["paper scissors rock","psr"]
 horror_story = ["horror story","game story","horror game","horror","horror story game"]
 bearbot_list = ["bearbot", "Bearbot"]
 chatterbot_list = ["chatterbot","Chatterbot"]
+quit_list = ["quit","quit code","quit all code","end code"]
+back_list = ["back","go back"]
 
 
 def gameoptions():
@@ -12,16 +14,22 @@ def gameoptions():
         import gamestory
     elif whatgame in paperscissorsrock:
         import paperscissorsrockgame
+    elif whatgame in quit_list:
+        quit()
+    elif whatgame in back_list:
+        choices()
     else:
         print("Please enter a valid response")
         gameoptions()
-print("Would you like to talk to Bearbot or play a Game?")
+print("Would you like to talk to Bearbot or play a Game? Please answer in lowercases.")
 def choices():
     choice = input()
     if choice in bearbot_list:
         import berabot
     elif choice in game_list:
         gameoptions()
+    elif choice in quit_list:
+        quit()
     else:
         print("Please enter a valid response")
         choices()
